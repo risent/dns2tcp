716e2368d40571670ead8e6e633acc519e432848
Risent Zhang <shengqi542@gmail.com>
2012-11-03 19:11:44 +0800

    Process the message compression in answers Resource Record.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ dns2tcp
 A DNS tcp proxy with some capture for practice.
 
 TODO
-====
+----
 
 1. Process the message compression in answers  Resource Record.
 2. DNSCrypt support.
--- dns2tcp.go
+++ dns2tcp.go
@@ -55,60 +55,70 @@ func Itob(x uint16) bool {
 	return true
 }
 
-func getQueryName(buf *bytes.Buffer) string {
+func getDomainName(data []byte, cursor int) (name string, offset int) {
 	over := false
 	n := 0
+	ptr := 0
+	length := len(data[cursor:])
 	labels := make([]string, 63)
-	log.Printf("input buf: %x", buf)
+
+Loop:
 	for ; !over; n++ {
-		labelsize, err := buf.ReadByte()
-		// detect message compression and ignore it in this version.
-		if labelsize == 0xc0 {
-			log.Printf("message compression")
-			var position uint8
-			err = binary.Read(buf, binary.BigEndian, &position)
+		if cursor >= len(data) {
+			return "", len(data)
+		}
+		labelsize := data[cursor]
+		cursor++
+		if labelsize == 0 {
+			break Loop
+		}
+
+		switch labelsize & 0xC0 {
+		case 0x00:
+			if labelsize == 0x00 {
+				break Loop
+			}
+			label := make([]byte, labelsize)
+			size, err := bytes.NewBuffer(data[cursor:]).Read(label)
+			// cursor++
 			if err != nil {
 				log.Fatal(err)
 			}
-			// if size != 1 {
-			// 	log.Print("Read postion error %d", size)
-			// }
-			return string(position)
+			if size != int(labelsize) {
+				log.Printf("Error in read %d, bytes of %x", size, label)
+			}
+			labels[n] = string(label)
+			cursor += size
+		case 0xC0:
+			n--
+			if ptr == 0 {
+				offset = cursor
+			}
+			if ptr > 10 {
+				return "Too many compression pointers", offset
+			}
+			ptr++
 
+			c1 := int(data[cursor])
+			cursor = (int(labelsize)^0xC0)<<8 | int(c1)
+		default:
+			return "", length
 		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		if labelsize == 0 {
-			over = true
-			break
-		}
-		label := make([]byte, labelsize)
-		size, err := buf.Read(label)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if size != int(labelsize) {
-			log.Printf("Error in read %d, bytes of %x", size, label)
-			log.Fatal()
-		}
-		labels[n] = string(label)
-		log.Print(label)
 	}
 
-	labels = labels[0:n]
-	name := strings.Join(labels, ".")
-	return name
+	name = strings.Join(labels[0:n], ".")
+	log.Printf("name=%s", name)
+	if ptr != 0 {
+		return name, offset + 1
+	}
+	return name, cursor
 }
 
-func parseDNSMsg(buf *bytes.Buffer) dnsMsg {
+func parseDNSMsg(data []byte) dnsMsg {
 	var msg dnsMsg
-	err := binary.Read(buf, binary.BigEndian, &msg.id)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var dnsmisc uint16
-	err = binary.Read(buf, binary.BigEndian, &dnsmisc)
+	msg.id = binary.BigEndian.Uint16(data)
+	// var dnsmisc uint16
+	dnsmisc := binary.BigEndian.Uint16(data[2:])
 	msg.response = true
 	msg.opcode = uint((dnsmisc >> 11) & 0x000F)
 	msg.authoritative = Itob((dnsmisc & 0x0400) >> 10)
@@ -117,30 +127,44 @@ func parseDNSMsg(buf *bytes.Buffer) dnsMsg {
 	msg.recursion_available = Itob((dnsmisc & 0x00F0) >> 7)
 	msg.rcode = uint(dnsmisc & 0x000F)
 
-	// var question_num uint16
-	err = binary.Read(buf, binary.BigEndian, &msg.question_num)
-	err = binary.Read(buf, binary.BigEndian, &msg.answer_num)
-	err = binary.Read(buf, binary.BigEndian, &msg.authority_num)
-	err = binary.Read(buf, binary.BigEndian, &msg.additional_num)
+	msg.question_num = binary.BigEndian.Uint16(data[4:])
+	msg.answer_num = binary.BigEndian.Uint16(data[6:])
+	msg.authority_num = binary.BigEndian.Uint16(data[8:])
+	msg.additional_num = binary.BigEndian.Uint16(data[10:])
 
 	question := make([]dnsQuestion, msg.question_num)
+	cursor := 12
 	for i := 0; i < int(msg.question_num); i++ {
-		question[i].Name = getQueryName(buf)
-		err = binary.Read(buf, binary.BigEndian, &question[i].Qtype)
-		err = binary.Read(buf, binary.BigEndian, &question[i].Qclass)
+		// var nameLength int
+		question[i].Name, cursor = getDomainName(data, cursor)
+		// cursor += offset
+		question[i].Qtype = binary.BigEndian.Uint16(data[cursor:])
+		cursor += 2
+		question[i].Qclass = binary.BigEndian.Uint16(data[cursor:])
+		cursor += 2
 	}
 	msg.question = question
+
 	if msg.answer_num > 0 {
+		log.Printf("answer number: %d cursor: %d", msg.answer_num, cursor)
 		answer := make([]dnsRR, msg.answer_num)
 		for i := 0; i < int(msg.answer_num); i++ {
-			answer[i].Name = getQueryName(buf)
-			err = binary.Read(buf, binary.BigEndian, &answer[i].Rrtype)
-			err = binary.Read(buf, binary.BigEndian, &answer[i].Class)
-			err = binary.Read(buf, binary.BigEndian, &answer[i].Ttl)
-			err = binary.Read(buf, binary.BigEndian, &answer[i].Rdlength)
+			answer[i].Name, cursor = getDomainName(data, cursor)
+			answer[i].Rrtype = binary.BigEndian.Uint16(data[cursor:])
+			cursor += 2
+			answer[i].Class = binary.BigEndian.Uint16(data[cursor:])
+			cursor += 2
+			answer[i].Ttl = binary.BigEndian.Uint32(data[cursor:])
+			cursor += 4
+			answer[i].Rdlength = binary.BigEndian.Uint16(data[cursor:])
+			cursor += 2
 			Data := make([]byte, answer[i].Rdlength)
-			err = binary.Read(buf, binary.BigEndian, &Data)
+			err := binary.Read(bytes.NewBuffer(data[cursor:]), binary.BigEndian, &Data)
+			if err != nil {
+				log.Fatal(err)
+			}
 			answer[i].Data = Data
+			cursor += int(answer[i].Rdlength)
 		}
 		msg.answer = answer
 	}
@@ -153,9 +177,7 @@ func dnsRequest(data []byte) []byte {
 		log.Fatal(err)
 	}
 
-	// buf := new(bytes.Buffer)
-	// err = binary.Write(buf, binary.BigEndian, uint16(len(data)))
-	query := parseDNSMsg(bytes.NewBuffer(data))
+	query := parseDNSMsg(data)
 	log.Printf("query: %v", query)
 	req := make([]byte, 2)
 	binary.BigEndian.PutUint16(req, uint16(len(data)))
@@ -179,7 +201,7 @@ func dnsRequest(data []byte) []byte {
 	}
 	defer conn.Close()
 
-	msg := parseDNSMsg(bytes.NewBuffer(reply[2 : length+2]))
+	msg := parseDNSMsg(reply[2 : length+2])
 	log.Printf("reply: %v", msg)
 	return reply[2 : length+2]
 }
@@ -192,14 +214,14 @@ func dnsListen(conn net.UDPConn) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Print("Data come in from", addr)
+	log.Printf("Data come in from: %s", addr)
 
 	reply := dnsRequest(buf[0:n])
 	_, err = conn.WriteTo(reply, addr)
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		log.Print("Reply sent")
+		log.Print("=====EOF=====")
 	}
 }
 
